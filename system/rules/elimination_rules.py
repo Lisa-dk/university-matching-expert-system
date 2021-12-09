f18cd154abea655e90e48a2054f13c222f518399
Lisa-dk <lisadkoopmans@gmail.com>
2021-12-09 20:24:07 +0100

    comments

--- system/rules/elimination_rules.py
+++ system/rules/elimination_rules.py
@@ -1,13 +1,17 @@
 def elimination_update(study, studies_kb, user_kb):
+
+    # matching subjects
     for subject in study['subjects']:
         if subject not in user_kb['subjects']:
             studies_kb.remove(study)
             return 
     
+    # matching english test
     if user_kb['english test'] != None and user_kb['english test'] not in study['english test']:
         studies_kb.remove(study)
         return
     
+    # matching english test overall score
     if (user_kb['english test'] != None and user_kb[(user_kb['english test'] + 'overall')] != '' and
                 user_kb[(user_kb['english test'] + 'overall')] in user_kb.keys() and 
                 user_kb[(user_kb['english test'] + 'overall')] in study.keys() and 
@@ -15,7 +19,7 @@ def elimination_update(study, studies_kb, user_kb):
         studies_kb.remove(study)
         return
 
-    
+    # matching english test minimum score
     if (user_kb['english test'] != None and (user_kb['english test'] + 'min') in user_kb.keys() and
                 user_kb[(user_kb['english test'] + 'min')] != '' and
                 user_kb[(user_kb['english test'] + 'min')] in user_kb.keys() and 
--- system/rules/question_rules.py
+++ system/rules/question_rules.py
@@ -2,6 +2,7 @@ import re
 
 def rules_check(answer, queries, study_kb, user_kb):
 
+    # Adding subjects to user facts
     if 'subjects' not in user_kb.keys():
         user_kb['subjects'] = []
         if re.search('chemistry', answer):
@@ -18,6 +19,7 @@ def rules_check(answer, queries, study_kb, user_kb):
         user_kb['english test'] = None
         queries.addQuestion("What English test did you take?")
 
+    # case for general english test
     if (answer in ['TOEFL', 'IELTS', 'CPE', 'CPA', 'TOEIC', 'IB A HL', 'Cambridge B2', 
                 'Cambridge C1', 'Cambridge C2', 'IB English programme', 'IB', 'EB']):
         user_kb['english test'] = answer
@@ -25,12 +27,14 @@ def rules_check(answer, queries, study_kb, user_kb):
 
     english_tests_specific = ['TOEFL', 'IELTS', 'CPE', 'CPA', 'TOEIC'] 
 
+    # If a minimum overall score is required, for any university, ask the overall score and add the field 
     if ((answer in english_tests_specific or answer in ['IB A HL', 'Cambridge B2', 'Cambridge C1', 'Cambridge C2']) and 
                 ((answer + 'overall') not in user_kb.keys())):
         user_kb[answer + 'overall'] = ''
         queries.addQuestion("What is the overall score of the English test?")
     
 
+    # if a minimum per section is required for an English test for any uni, ask the question and add the field
     if (answer in english_tests_specific and 
                 (answer + 'min') not in user_kb.keys()):
         user_kb[answer + 'min'] = ''
