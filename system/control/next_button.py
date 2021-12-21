0517a2d8b08667042abdecd221939375c105a95e
Lisa-dk <lisadkoopmans@gmail.com>
2021-12-21 19:14:49 +0100

    added comments + fixed small bug

--- system/control/next_button.py
+++ system/control/next_button.py
@@ -1,15 +1,21 @@
 from model.rules.question_rules import rules_check
 from model.rules.elimination_rules import elimination_update
 
+# Updates the question list by iterating over all rules and updating the question textfield.
 def loadNewQuestion(questionField, kbClass, questionClass, question, inputField):
+    # obtaining the input
     input = inputField.inputText.get("1.0", "end-1c")
+
+    # checking over the rules and removing the question
     rules_check(input, questionClass, kbClass.kb, kbClass.user_kb)
     questionClass.removeQuestion(question)
     eliminate_studies(kbClass)
+    # noting whether there are questions left.
     EMPTY = 0 if len(questionClass.getQuestions()) > 0 else 1    
     questionField.update(EMPTY)
 
-    
+
+# Eliminates studies from the kb
 def eliminate_studies(kbClass):
         size_kb = len(kbClass.kb)
         i = 0
--- system/view/question_field.py
+++ system/view/question_field.py
@@ -10,45 +10,51 @@ class QuestionField():
         self.kbClass = kbClass
 
         self.addTextField(self.master)
-        self.setText(self.question)
+        self.addText(self.question)
 
         self.inputField = InputFields(self.master)
 
         self.addSaveButton(self.master)
     
+
     def addTextField(self, frame):
         self.textField = Text(frame, height=5, width=100)
         self.textField.pack()
 
-    def setText(self, text):
+
+    def addText(self, text):
         self.textField.insert(END, text)
     
+
     def addSaveButton(self, frame):
         self.button = Button(frame, height=1, width=10, text="Next", 
                             command=lambda: loadNewQuestion(self, self.kbClass, self.questionClass, self.question, self.inputField))
         self.button.pack()
 
+    # updates the question and input text fields
     def update(self, EMPTY):
         self.textField.delete("1.0", "end-1c")
+        self.inputField.inputText.delete("1.0", "end-1c")
 
         if len(self.kbClass.kb) == 0:
-                self.setText("No requirements are met. No appropriate study programmes available.")
+                self.addText("No requirements are met. No appropriate study programmes available.")
 
         if not EMPTY and len(self.kbClass.kb) > 0:
             self.question = self.questionClass.getQuestions()[0]
-            self.setText(self.question)
+            self.addText(self.question)
         else:
             self.inputField.inputText.destroy()
             self.button.destroy()
             self.give_results()
+
         
-        self.inputField.inputText.delete("1.0", "end-1c")
-    
+    # prints the studies for which the requirements are met.
     def give_results(self):
+        results = "Requirements met for the following studies:\n"
         if len(self.kbClass.kb) > 0:
-            self.setText("Requirements met for the following studies:\n")
-            for study in self.kb:
-                    self.setText(study['label'] + " at " + study['university'] + "\n")
+            for study in self.kbClass.kb:
+                results += study['label'] + " at " + study['university'] + "\n"
+        self.addText(results)
             
 
 class InputFields():
