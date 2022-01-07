b23a13d2901bd241c910ee1d61a4206ac0b76fd7
ekinfergan <ferganekin@gmail.com>
2022-01-07 16:32:15 +0300

    tried to add a scrollbar

--- system/view/question_field.py
+++ system/view/question_field.py
@@ -19,7 +19,31 @@ class QuestionField:
         self.button = None
         self.question_field = None
         self.frame = Frame(self.master, bg=Theme.BG_COLOUR)
-        self.frame.pack()
+        #self.frame.pack()
+        
+        # TODO: Scrollbar needs canvas appereantly...
+        self.canvas =  Canvas(self.frame)
+        
+
+        self.scrollbar = Scrollbar(self.frame, orient="vertical", command=self.canvas.yview)
+        
+        self.scrollable_frame = Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+        
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.frame.pack(fill="both", expand=True)
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
         self.question_text = StringVar()
         self.question_class = Question()
         self.disclaimer = Disclaimer()
@@ -27,17 +51,17 @@ class QuestionField:
         self.question = self.question_class.get_questions()[0]
         self.kb_class = kb_class
 
-        self.add_question_field(self.frame)
+        self.add_question_field(self.scrollable_frame)
         self.add_text(self.question[0])
 
         self.input_field = None
         self.options = None
         self.add_input_field()
 
-        self.add_save_button(self.frame)
+        self.add_save_button(self.scrollable_frame)
 
-    def add_question_field(self, frame):
-        self.question_field = Label(frame, textvariable=self.question_text, height=3, width=100, wraplength=500,
+    def add_question_field(self, scrollable_frame):
+        self.question_field = Label(scrollable_frame, textvariable=self.question_text, height=3, width=100, wraplength=500,
                                     justify=LEFT, font=Font(family="Arial"), fg=Theme.TEXT_COLOUR, bg=Theme.BG_COLOUR,
                                     bd=0)
         self.question_field.pack()
@@ -45,8 +69,8 @@ class QuestionField:
     def add_text(self, text):
         self.question_text.set(text)
 
-    def add_save_button(self, frame):
-        self.button = Button(frame, height=1, width=10, text="Next",
+    def add_save_button(self, scrollable_frame):
+        self.button = Button(scrollable_frame, height=1, width=10, text="Next",
                              command=lambda: load_new_question(self, self.kb_class, self.question_class, self.question,
                                                                self.input_field), bg=Theme.BUTTON_COLOUR, bd=1,
                              activebackground=Theme.BUTTON_CLICK)
@@ -59,16 +83,16 @@ class QuestionField:
         if self.question[1] == QuestionType.SELECT:
             # self.options = ['Lise Diploma', 'AP', 'IB', 'Label France Education', 'British GCE A Levels']
             # self.input_field = RadioButtonField(self.frame, self.options)
-            self.input_field = RadioButtonField(self.frame, self.question[2])
+            self.input_field = RadioButtonField(self.scrollable_frame, self.question[2])
 
         # elif 'subject' in self.question:
         elif self.question[1] == QuestionType.MULTI_SELECT:
             # self.options = ['Analytics & Approaches SL', 'Analytics & Approaches HL', 'Mathematics SL', 'Mathematics HL', 'Calculus', 'Physics SL', 'Physics HL', 'Chemistry SL', 'Chemistry HL']
             # self.input_field = CheckButtonField(self.frame, self.options)
-            self.input_field = CheckButtonField(self.frame, self.question[2])
+            self.input_field = CheckButtonField(self.scrollable_frame, self.question[2])
 
         else:  # if self.question[1] == QuestionType.TEXT_FIELD:
-            self.input_field = TextFields(self.frame)
+            self.input_field = TextFields(self.scrollable_frame)
 
     def destroy(self):
         self.question_field.destroy()
