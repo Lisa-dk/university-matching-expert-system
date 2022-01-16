4f92ab0837e2706aff69e11dfbc52d7698a1315d
Sophie Tijssen <s.y.tijssen@student.rug.nl>
2022-01-16 18:11:58 +0100

    Fixed the text input so now only one line can be edited.

--- system/view/inputs/text_input.py
+++ system/view/inputs/text_input.py
@@ -18,24 +18,23 @@ class TextFields:
         :param frame: frame to add text field to
         """
         # self.input_text = Text(frame, height=1, width=5, font=('Arial', 16), fg=Theme.TEXT_COLOUR, bg=Theme.BG_COLOUR, bd=1, wrap='word')
-
         self.input_widget = Entry(frame, width=5, font=('Arial', 16), fg=Theme.TEXT_COLOUR, bg=Theme.BG_COLOUR, bd=1, textvariable=self.input_text)
         self.input_widget.focus_set()
         self.input_widget.pack(padx=10, pady=5)
 
-        def character_limit(entry_text):
-            if len(entry_text.get()) > 0:
-                entry_text.set(entry_text.get()[-1])
+        # def character_limit(entry_text):
+        #     if len(entry_text.get()) > 0:
+        #         entry_text.set(entry_text.get()[-1])
 
-        self.input_text.trace("w", lambda *args: character_limit(self.input_text))
+        # self.input_text.trace("w", lambda *args: character_limit(self.input_text))
 
     def get_chosen_option(self):
         """
         Gets contents text field
         :return: contents text field (as str)
         """
-        print(self.input_widget.get()[-1])
-        return self.input_widget.get()[-1]
+        print(self.input_widget.get())
+        return self.input_widget.get()
     
     def destroy(self):
         self.input_widget.destroy()
